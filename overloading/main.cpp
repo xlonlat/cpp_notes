48778218e13d169c281ac906db9e8c96374966e7
xlonlat <xlonlat@126.com>
2022-04-20 09:20:39 +0800

    Added two notes.

---
+++ overloading/main.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+
+class MulitInt
+{
+public :
+    int     m;
+    int     n;
+
+    MulitInt() { m = n = 0; }
+
+    MulitInt(int x, int y) : m(x), n(y) {}
+
+    // 支持了+号运算符
+    MulitInt operator +(MulitInt val) const
+    {
+        return MulitInt(m + val.m, n + val.n);
+    }
+
+    void Add(int x)
+    {
+        m += x;
+        n += x;
+    }
+
+    void Add(int x, int y)
+    {
+        m += x;
+        n += y;
+    }
+};
+
+
+int main()
+{
+    //////////////////////////////////////////////////////////
+    // 测试运算符重载
+    MulitInt mi1(1, 2);
+    MulitInt mi2(3, 4);
+    MulitInt res = mi1 + mi2;
+
+    //////////////////////////////////////////////////////////
+    // 测试函数重载
+    MulitInt mi3;
+    mi3.Add(1);
+    mi3.Add(2, 3);
+
+    int i;
+    std::cin >> i;
+}
---
+++ override/override.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+
+class Base
+{
+public:
+    virtual void Print(int x) const
+    {
+        std::cout << x << std::endl;
+    }
+};
+
+class Balabala : public Base
+{
+public:
+    // 编译不通过！基类根本没有这个函数。
+    //virtual void Printf(int x) override
+    //{
+    //    std::cout << "Balabala:" << x << std::endl;
+    //}
+
+    // 编译不通过!函数名一致但参数不一致。
+    //virtual void Printf(int x, int y) override
+    //{
+    //    std::cout << x << "-" << y << std::endl;
+    //}
+
+    // 编译不通过！常量性不一致。（人话：基类加了const修饰）
+    //virtual void Print(int x)  override
+    //{
+    //    std::cout << "Balabala:" << x << std::endl;
+    //}
+
+    // 说白了加了override就是要求你对于基类的虚函数声明一定要保持一致，当然基类也要有同名函数，然后再考虑如何重新实现，就是重写或覆盖。
+    virtual void Print(int x) const override
+    {
+    std::cout << "Balabala:" << x * 2 << std::endl;
+    }
+
+};
+
+int main()
+{
+    Balabala bala;
+    bala.Print(1);
+
+    int i;
+    std::cin >> i;
+}
